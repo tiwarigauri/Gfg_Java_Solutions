5cd74369621d401fbbde1461731ed3a307c01626
tiwarigauri <gauri.uiet@gmail.com>
2023-10-24 18:50:08 +0530

    modified the code

--- readPrintIntValue.java
+++ readPrintIntValue.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 class readPrintIntValue {
     public static void main(String[] args) {
+        
+        System.out.print("Enter the integer: ");
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        System.out.println("Enter the integer: " + x);
         sc.close();
         System.out.println("Entered integer is: " + x);
     }
